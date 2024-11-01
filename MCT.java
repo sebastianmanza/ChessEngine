46d9fd1e7a1cbdfffcd6dcf4ad455e4e1178d8bb
Sebastian <manzaseb@grinnell.edu>
2024-11-01 13:02:42 -0500

    Added to the Tree-search

--- MCT.java
+++ MCT.java
@@ -1,6 +1,7 @@
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Random;
+import java.lang.Math;
 
 import utils.MCTUtils.MCNode;
 import utils.MCTUtils.move;
@@ -10,6 +11,8 @@ import utils.MCTUtils.move;
  * @author Sebastian Manza
  */
 public class MCT {
+    /** The exploration parameter, used to balance exploration vs exploitation */
+    public static final double EXPLORATION_PARAM = Math.sqrt(2);
     /** The root node of the move. */
     MCNode root;
 
@@ -39,41 +42,57 @@ public class MCT {
         }
          return bestMove;
     }
+
+    /**
+     * Calculates a value for a node to select
+     * @param node The node to calculate
+     * @return The value of the node
+     */
+    public static double UCT(MCNode node) {
+        double result = node.totalWins / node.totalPlayOuts + EXPLORATION_PARAM * Math.sqrt((Math.log(node.lastMove.totalPlayOuts)) / node.totalPlayOuts);
+        return result;
+    } //UCT(node)
     
     /**
      * Selects the best possible node from the current root with current knowledge.
      * @param node The beginning node.
      * @return The best possible node from the beginning node.
      */
-    public MCNode select(MCNode node) {
-        return node; //STUB
-    }
+    public static MCNode select(MCNode root) {
+        MCNode curNode = root;
+        while (hasExploredChildren) {
+            curNode = Collections.max(curNode.nextMoves, Comparator.comparingDouble(n -> UCT(n)));
+        } //while
+        return curNode;
+    } // select(root)
 
     /**
      * Expands the tree one level deeper to continue searching
      * @param node The first node reached with no children.
      * @return The expanded node.
      */
-    public MCNode expand(MCNode node) {
-        return node; //STUB
-    }
+    public static MCNode expand(MCNode node) {
+        Random randomChild = new Random();
+        /* Grab a random child from the tree */
+        return node.nextMoves.get(randomChild.nextInt(node.nextMoves.size()));
+    } //expand(node)
 
     /**
      * Randomly simulates the finish of the game from the current game state. 
      * Note: current implementation creates lists of all future moves and chooses randomly. 
      * Could be made more efficient by creating just one random move?
-     * @param node The terminating node
+     * @param node The terminating nodegf
      * @return the number of win-points
      */
-    public double simulate(MCNode node) {
+    public static double simulate(MCNode node) {
         Random ranMove = new Random();
         MCNode curMove = node;
         /* Run the loop while the game is undecided */
         while(!gameState.gameFinished(curMove)) {
             /* take a random move and play from that position */
-            curMove = curMove.nextMoves().get(ranMove.nextInt(curMove.nextMoves().size()));
+            curMove = curMove.nextMoves.get(ranMove.nextInt(curMove.nextMoves.size()));
         } // while
-        return gameState.vicPoints()
+        return curMove.vicPoints();
     } //simulate
 
     /**
--- utils/MCTUtils/MCNode.java
+++ utils/MCTUtils/MCNode.java
@@ -1,20 +1,18 @@
 package utils.MCTUtils;
 import java.util.List;
 public class MCNode {
-    move currentMove;
-    List<MCNode> nextMoves;
-    double totalWins;
-    int totalPlayOuts;
-    MCNode lastMove;
+    public move currentMove;
+    public List<MCNode> nextMoves;
+    public double totalWins;
+    public int totalPlayOuts;
+    public MCNode lastMove;
 
     public MCNode(move move) {
         this.currentMove = move;
+
     }
 
-    /** Returns a list of all possible next moves */
-    public List<MCNode> nextMoves() {
-        return nextMoves;
-    } //nextMoves
+    
 
 
 }
